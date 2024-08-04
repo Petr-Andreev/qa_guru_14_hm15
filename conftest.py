a327ec2bb5cf1c1c4c89d7471d4a8327926a6459
Petr Andreev <drakula2805@gmail.com>
2024-08-04 15:07:29 +0300

    Fix

--- conftest.py
+++ conftest.py
@@ -2,22 +2,24 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope='function', params=[(1920, 1080), (1280, 720), (430, 932)])
-def setup_browser(request):
+@pytest.fixture(scope='function', params=[(1920, 1080), (1280, 720), (390, 844)],
+                ids=['Full HD', 'HD', 'IPhone 12 Pro'])
+def setup_browser_management(request):
     width, height = request.param
-
     browser.config.window_width = width
     browser.config.window_height = height
     browser.config.base_url = 'https://github.com'
 
     if width > 800:
         yield 'desktop'
+    else:
         yield 'mobile'
 
     browser.quit()
 
 
-@pytest.fixture(scope='function', params=[(430, 932), (390, 844), (360, 740)])
+@pytest.fixture(scope='function', params=[(430, 932), (390, 844), (360, 740)],
+                ids=['IPhone 14 Pro Max', 'IPhone 12 Pro', 'Galaxy S8+'])
 def setup_browser_mobile(request):
     width, height = request.param
     browser.config.window_height = height
@@ -29,7 +31,8 @@ def setup_browser_mobile(request):
     browser.quit()
 
 
-@pytest.fixture(scope='function', params=[(3840, 2160), (1920, 1080), (1280, 1024)])
+@pytest.fixture(scope='function', params=[(1020, 720), (1920, 1080), (2560, 1440)],
+                ids=['HD', 'Full HD', 'QHD'])
 def setup_browser_desktop(request):
     width, height = request.param
     browser.config.window_height = height
--- tests/test_github_different_methods.py
+++ tests/test_github_different_methods.py
@@ -3,18 +3,18 @@ from selene import browser
 from pages.auth_page import auth_page
 
 
-def test_sign_in(setup_browser):
+def test_sign_in(setup_browser_management):
     auth_page.sign_in_desktop()
 
 
-def test_sign_in_desktop(setup_browser):
-    if setup_browser == 'mobile':
+def test_sign_in_desktop(setup_browser_management):
+    if setup_browser_management == 'mobile':
         pytest.skip(reason='Разрешение экрана - для мобильной версии, поэтому тест пропущен.')
     auth_page.sign_in_desktop()
 
 
-def test_sign_in_mobile(setup_browser):
-    if setup_browser == 'desktop':
+def test_sign_in_mobile(setup_browser_management):
+    if setup_browser_management == 'desktop':
         pytest.skip(reason='Разрешение экрана - для десктопной версии, поэтому тест пропущен.')
     auth_page.sign_in_mobile()
 
