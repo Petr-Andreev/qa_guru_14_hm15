e839d377676ddb1faa268c622f7258ebf113b8cd
Petr Andreev <drakula2805@gmail.com>
2024-08-06 11:11:14 +0300

    Fix

--- conftest.py
+++ conftest.py
@@ -1,17 +1,18 @@
 import pytest
 from selene import browser
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function', params=[(1920, 1080), (1280, 720), (390, 844), (360, 740)],
                 ids=['Full HD', 'HD', 'IPhone 12 Pro', 'Galaxy S8+'])
 def setup_browser_management(request):
+    browser.config.base_url = 'https://github.com'
     width, height = request.param
     browser.config.window_width = width
     browser.config.window_height = height
-    browser.config.base_url = 'https://github.com'
-    options = Options()
-    options.page_load_strategy.page_load_strategy = 'eager'
+    driver_options = webdriver.ChromeOptions()
+    browser.config.driver_options = driver_options
+    driver_options.page_load_strategy = 'eager'
 
     if width >= 800:
         yield 'desktop'
@@ -28,8 +29,9 @@ def setup_browser_mobile(request):
     browser.config.window_height = height
     browser.config.window_width = width
     browser.config.base_url = 'https://github.com'
-    options = Options()
-    options.page_load_strategy.page_load_strategy = 'eager'
+    driver_options = webdriver.ChromeOptions()
+    browser.config.driver_options = driver_options
+    driver_options.page_load_strategy = 'eager'
 
     yield
 
@@ -43,8 +45,9 @@ def setup_browser_desktop(request):
     browser.config.window_height = height
     browser.config.window_width = width
     browser.config.base_url = 'https://github.com'
-    options = Options()
-    options.page_load_strategy.page_load_strategy = 'eager'
+    driver_options = webdriver.ChromeOptions()
+    browser.config.driver_options = driver_options
+    driver_options.page_load_strategy = 'eager'
 
     yield
 
